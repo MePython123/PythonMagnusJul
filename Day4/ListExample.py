0706e5d423ec6f055ab7265fa9480eae9e63ba48
MePython123 <python.devops123@gmail.com>
2023-07-27 16:51:04 -0400

    Day4_26_July_2023

---
+++ Day4/ListExample.py
@@ -0,0 +1,21 @@
+a1 = [10, 30, 23, 'abc', 10.5, 'hello']
+
+print(a1)
+print("welcome to Python")
+print(a1[0:3])
+print(a1[3:6])
+print(a1[2:])
+print(a1[:5])
+print(a1)
+a1.append(120) # Appending the data to List
+print(a1)
+a1.insert(3,'hi') # Inserting the value at specific position
+print(a1)
+a1[6] = "welcome" # Replacing the value at specific position
+print(a1)
+a1.remove('abc')
+print(a1)
+a1.pop()
+print(a1)
+a1.pop(4)
+print(a1)
